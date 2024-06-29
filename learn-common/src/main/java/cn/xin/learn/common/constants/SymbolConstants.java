ec8ee781aa9dd179a4b5091daa8e4a350daf1dc0
tiancx <813876299@qq.com>
2024-06-29 19:40:25 +0800

    feat:add learn-common module

---
+++ learn-common/src/main/java/cn/xin/learn/common/constants/SymbolConstants.java
@@ -0,0 +1,54 @@
+package cn.xin.learn.common.constants;
+
+/**
+ * 符号常量
+ * @author tiancx
+ */
+public interface SymbolConstants {
+
+        /**
+        * 逗号
+        */
+        String COMMA = ",";
+
+        /**
+        * 点
+        */
+        String DOT = ".";
+
+        /**
+        * 冒号
+        */
+        String COLON = ":";
+
+        /**
+        * 分号
+        */
+        String SEMICOLON = ";";
+
+        /**
+        * 下划线
+        */
+        String UNDERLINE = "_";
+
+        /**
+        * 中划线
+        */
+        String MIDDLELINE = "-";
+
+        /**
+        * 竖线
+        */
+        String VERTICAL = "|";
+
+        /**
+        * 空格
+        */
+        String SPACE = " ";
+
+        /**
+        * 空字符串
+        */
+        String EMPTY = "";
+
+}
