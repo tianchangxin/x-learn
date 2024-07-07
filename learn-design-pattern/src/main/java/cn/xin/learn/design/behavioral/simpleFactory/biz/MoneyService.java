215081c44a6a8b17a20854d1d32e220b2f1c81f8
tiancx <813876299@qq.com>
2024-07-07 11:47:05 +0800

    feat:add log4j2

--- learn-design-pattern/src/main/java/cn/xin/learn/design/behavioral/simpleFactory/biz/MoneyService.java
+++ learn-design-pattern/src/main/java/cn/xin/learn/design/behavioral/simpleFactory/biz/MoneyService.java
@@ -1,22 +1,26 @@
 package cn.xin.learn.design.behavioral.simpleFactory.biz;
 
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author tiancx
  * @description: 金钱服务-简单工厂模式
  * @date 2024年06月30日
  */
+@Slf4j
 public class MoneyService {
     /**
      * 充值
      */
     public void recharge() {
-        System.out.println("充值");
+        log.info("充值");
     }
 
     /**
      * 提现
      */
     public void withdraw() {
-        System.out.println("提现");
+        log.info("提现");
     }
 }
