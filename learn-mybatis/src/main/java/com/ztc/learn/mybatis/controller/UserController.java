f6df7f89a27ed5535e3e13ebe8ff18d12d75d106
zhengtianci <ztc.laiyin@qq.com>
2024-08-20 14:51:52 +0800

    mybatis

---
+++ learn-mybatis/src/main/java/com/ztc/learn/mybatis/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.ztc.learn.mybatis.controller;
+
+import com.ztc.learn.mybatis.modle.domain.User;
+import com.ztc.learn.mybatis.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @GetMapping(value = "role")
+    public List<User> getUserByRole(String role){
+        return userService.getUserByRole(role);
+    }
+}
